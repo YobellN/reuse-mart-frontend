5e11e71027c3e7b739ed71d336b53b46189d85d3
Dina Oktavia Pangaribuan <140843745+sg-riley@users.noreply.github.com>
2025-05-01 04:53:24 +0700

    perbaikan

--- src/components/home/home-navbar-with-search.tsx
+++ src/components/home/home-navbar-with-search.tsx
@@ -35,26 +35,26 @@ export default function HomeNavbar({ user, ...props }: {user?: IResponse<User>})
             </div>
           </div>
 
-          <div className="flex h-16 w-25 items-center place-content-between">
-            <div className="h-10 m-1 w-10 bg-white hover:bg-slate-100 rounded-full py-1 shadow-sm flex items-center justify-center ">
+          <div className="flex h-16 w-30 items-center justify-end">
+            <div className="h-10 me-2 w-10 bg-white hover:bg-slate-100 rounded-full py-1 shadow-sm flex items-center justify-center ">
               <Link href="#">
-                <ShoppingCart className="text-lime-600 stroke-2" />
+                <ShoppingCart className="text-green-600 me-[1px] stroke-2" />
               </Link>
             </div>
+
             {user === undefined ? (
-                <Link href="/login">
+              <div className="h-10 ms-2 w-16 bg-white hover:bg-slate-100 rounded-md py-1 shadow-sm flex items-center justify-center ">
+                <Link href="/login" className="text-green-600 font-medium">
                   Login
                 </Link>
+              </div>
             ) : (
               <div className="h-10 m-1 w-10  bg-white hover:bg-slate-100 rounded-full py-1 shadow-sm flex items-center justify-center">
                 <Link href="/profile">
-                  <Avatar className=" rounded-lg grayscale ">
-                    <AvatarImage
-                      src={user.data?.nama}
-                      alt={user.data?.nama}
-                    />
-                    <AvatarFallback className="rounded-lg font-medium text-black">
-                      {user? user.data?.nama.charAt(0) : "G"}
+                  <Avatar className=" rounded-lg ">
+                    <AvatarImage src={user.data?.nama} alt={user.data?.nama} />
+                    <AvatarFallback className="rounded-lg font-semibold text-green-600">
+                      {user ? user.data?.nama.charAt(0) : "G"}
                     </AvatarFallback>
                   </Avatar>
                 </Link>
