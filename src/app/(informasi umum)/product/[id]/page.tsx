0cc29b3f94487430d058791763bafaa9506d7750
Dina Oktavia Pangaribuan <140843745+sg-riley@users.noreply.github.com>
2025-05-09 01:29:45 +0700

    Update page.tsx

--- src/app/(informasi umum)/product/[id]/page.tsx
+++ src/app/(informasi umum)/product/[id]/page.tsx
@@ -1,21 +1,26 @@
-import ShowDiskusiPage from "@/components/diskusi-produk/show-diskusi-page";
+"use server";
+
 import BreadcrumbsProduct from "@/components/product/breadcrumb-product";
 import DisplayProdukPenitip from "@/components/product/display-product-penitip";
 import ProductDetail from "@/components/product/product-detail";
 import SellerInfo from "@/components/product/seller-info-dan-chat";
+import { getProdukById } from "@/services/produk/produk-services";
+import { Produk } from "@/services/produk/schema-produk";
 
-export default function ProductDetailPage({
+export default async function ProductDetailPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = await params;
+  const produk: Produk | null = await getProdukById(id).catch(() => null);
+  if (!produk) return <div>Produk tidak ditemukan</div>;
   return (
     <div>
       <BreadcrumbsProduct />
-      <ProductDetail id={params.id} />
-      <SellerInfo />
-      <ShowDiskusiPage />
-      <DisplayProdukPenitip />
+      <ProductDetail {...produk} />
+      <SellerInfo idPenitip={produk.id_penitip} />
+      <DisplayProdukPenitip id_penitip={produk.id_penitip} />
     </div>
   );
 }
