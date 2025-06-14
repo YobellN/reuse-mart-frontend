ba607c77732655bc205583287874e3499ad373dc
YobellN <yobelnugroho17@gmail.com>
2025-06-14 14:55:03 +0700

    Update columns.tsx

--- src/app/(dashboard)/penitip/riwayat-pengambilan/columns.tsx
+++ src/app/(dashboard)/penitip/riwayat-pengambilan/columns.tsx
@@ -45,7 +45,7 @@ export const columns: ColumnDef<DetailProdukTitipan>[] = [
             accessorKey: "detail_penitipan.penitipan.tanggal_penitipan",
             header: "Tanggal Penitipan",
             accessorFn: (row) =>
-                row.detail_penitipan.penitipan.tanggal_penitipan
+                row.detail_penitipan?.penitipan.tanggal_penitipan
                     ? format(new Date(row.detail_penitipan.penitipan.tanggal_penitipan), "dd MMMM yyyy", { locale: id })
                     : "",
             cell: ({ row }) => {
@@ -57,7 +57,7 @@ export const columns: ColumnDef<DetailProdukTitipan>[] = [
         accessorKey: "detail_penitipan.penitipan.tenggat_pengambilan",
         header: "Tenggat Pengambilan",
         accessorFn: (row) =>
-            row.detail_penitipan.penitipan.tenggat_pengambilan
+            row.detail_penitipan?.penitipan.tenggat_pengambilan
                 ? format(new Date(row.detail_penitipan.penitipan.tenggat_pengambilan), "dd MMMM yyyy", { locale: id })
                 : "",
         cell: ({ row }) => {
@@ -69,7 +69,7 @@ export const columns: ColumnDef<DetailProdukTitipan>[] = [
         accessorKey: "tanggal_pengambilan",
         header: "Tanggal Pengambilan",
         accessorFn: (row) =>
-            row.detail_penitipan.tanggal_pengambilan
+            row.detail_penitipan?.tanggal_pengambilan
                 ? format(new Date(row.detail_penitipan.tanggal_pengambilan), "dd MMMM yyyy", { locale: id })
                 : "Belum diambil",
         cell: ({ row }) => {
